5c3e479e5614d98391357ed508d3b6b3fbd157a2
mwnmoa <mwnmoa@gmail.com>
2022-01-20 19:36:43 +0700

    Video 18

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 
 function App() {
-
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="content">
-        <Home/>
-        
+        <Home />
       </div>
     </div>
   );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,27 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Home Page</h2>
-        </div>
-      );
-}
- 
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
+    </div>
+  );
+};
+
 export default Home;

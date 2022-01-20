d5cdf2c45ed8e4fd7b053cecae0290fd09a52585
mwnmoa <mwnmoa@gmail.com>
2022-01-20 22:15:12 +0700

    Video 24

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Components/Create";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/Components/useFetch.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsPending(false);
+      });
+  }, []);
+
+  return {
+    data,
+    isPending,
+    error,
+  };
+};
+
+export default useFetch;
